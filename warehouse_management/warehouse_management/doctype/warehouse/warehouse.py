6276375e02b78a4e0b08e9331483e07699a78a16
Sabu Siyad <hello@ssiyad.com>
2022-10-10 11:04:09 +0530

    doctype(new): warehouse
    Signed-off-by: Sabu Siyad <hello@ssiyad.com>

---
+++ warehouse_management/warehouse_management/doctype/warehouse/warehouse.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Sabu Siyad and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+class Warehouse(NestedSet):
+	pass
