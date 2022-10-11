104b493871b155420d7d8f501fb9c51c59e9cefb
Sabu Siyad <hello@ssiyad.com>
2022-10-11 20:19:42 +0530

    feat: basic ledger report
    Signed-off-by: Sabu Siyad <hello@ssiyad.com>

---
+++ warehouse_management/warehouse_management/report/ledger_report/ledger_report.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2022, Sabu Siyad and contributors
+# For license information, please see license.txt
+
+import frappe
+
+def execute(filters=None):
+	columns = [
+		{
+			"fieldname": "item",
+			"fieldtype": "Link",
+			"label": "Item",
+			"options": "Item",
+		},
+		{
+			"fieldname": "warehouse",
+			"fieldtype": "Link",
+			"label": "Warehouse",
+			"options": "Warehouse",
+		},
+		{
+			"fieldname": "quantity_change",
+			"fieldtype": "Int",
+			"label": "Change in Quantity",
+			"indicator": "Green"
+		},
+		{
+			"fieldname": "value_change",
+			"fieldtype": "Currency",
+			"label": "Change in Value",
+		},
+	]
+
+	data = frappe.db.get_list("Warehouse Ledger", filters=filters, fields="*")
+
+	return columns, data
