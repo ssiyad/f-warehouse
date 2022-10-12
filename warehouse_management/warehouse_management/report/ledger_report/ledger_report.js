9a1e9948bbd14e4cd5e208a90db4d515ca2be629
Sabu Siyad <hello@ssiyad.com>
2022-10-12 16:30:53 +0530

    feat(ledger): a better (?) view
    Signed-off-by: Sabu Siyad <hello@ssiyad.com>

--- warehouse_management/warehouse_management/report/ledger_report/ledger_report.js
+++ warehouse_management/warehouse_management/report/ledger_report/ledger_report.js
@@ -3,12 +3,46 @@
 /* eslint-disable */
 
 frappe.query_reports["Ledger Report"] = {
+	"filters": [
+		{
+			"fieldname": "date_from",
+			"label": "From Date",
+			"fieldtype": "Datetime",
+		},
+		{
+			"fieldname": "date_to",
+			"label": "To Date",
+			"fieldtype": "Datetime",
+		},
+		{
+			"fieldname": "item",
+			"label": "Item",
+			"fieldtype": "Link",
+			"options": "Item",
+		},
+		{
+			"fieldname": "Warehouse",
+			"label": "warehouse",
+			"fieldtype": "Link",
+			"options": "Warehouse",
+		},
+		{
+			"fieldname": "stock_entry",
+			"label": "Stock Entry",
+			"fieldtype": "Link",
+			"options": "Stock Entry",
+		},
+	],
 	"formatter": function(value, row, column, data, default_formatter) {
 		value = default_formatter(value, row, column, data);
 
-		if (column.id == "value_change" && data) {
+		if (["value_change", "quantity_change"].includes(column.id) && data) {
 			color = data.value_change < 0 ? "red" : "green";
 			value = `<span style='color:${color};'>${value}</span>`;
+		}
+
+		if (row === undefined && column.id === "unit_value") {
+			value = ""
 		}
 
 		return value;
--- warehouse_management/warehouse_management/report/ledger_report/ledger_report.py
+++ warehouse_management/warehouse_management/report/ledger_report/ledger_report.py
@@ -3,38 +3,73 @@
 
 import frappe
 
+
 def execute(filters=None):
-	columns = [
-		{
-			"fieldname": "modified",
-			"fieldtype": "Datetime",
-			"label": "Date",
-		},
-		{
-			"fieldname": "item",
-			"fieldtype": "Link",
-			"label": "Item",
-			"options": "Item",
-		},
-		{
-			"fieldname": "warehouse",
-			"fieldtype": "Link",
-			"label": "Warehouse",
-			"options": "Warehouse",
-		},
-		{
-			"fieldname": "quantity_change",
-			"fieldtype": "Int",
-			"label": "Change in Quantity",
-			"indicator": "Green"
-		},
-		{
-			"fieldname": "value_change",
-			"fieldtype": "Currency",
-			"label": "Change in Value",
-		},
-	]
+    columns = [
+        {
+            "fieldname": "modified",
+            "fieldtype": "Datetime",
+            "label": "Date",
+        },
+        {
+            "fieldname": "item",
+            "fieldtype": "Link",
+            "label": "Item",
+            "options": "Item",
+        },
+        {
+            "fieldname": "warehouse",
+            "fieldtype": "Link",
+            "label": "Warehouse",
+            "options": "Warehouse",
+        },
+        {
+            "fieldname": "quantity_before",
+            "fieldtype": "Int",
+            "label": "Quantity Before",
+        },
+        {
+            "fieldname": "quantity_change",
+            "fieldtype": "Int",
+            "label": "Change in Quantity",
+        },
+        {
+            "fieldname": "quantity_after",
+            "fieldtype": "Int",
+            "label": "Quantity After",
+        },
+        {
+            "fieldname": "unit_value",
+            "fieldtype": "Currency",
+            "label": "Value per Unit",
+        },
+        {
+            "fieldname": "value_change",
+            "fieldtype": "Currency",
+            "label": "Change in Value",
+        },
+    ]
 
-	data = frappe.db.get_list("Warehouse Ledger", filters=filters, fields="*")
+    if filters and "date_from" in filters:
+        filters["modified"] = [">", filters["date_from"]]
+        del filters["date_from"]
 
-	return columns, data
+    if filters and "date_to" in filters:
+        filters["modified"] = ["<", filters["date_to"]]
+        del filters["date_to"]
+
+    data = frappe.db.get_list("Warehouse Ledger", filters=filters, fields="*")
+
+    for row in data:
+        row["quantity_before"] = (
+            frappe.db.get_value(
+                "Warehouse Ledger",
+                {**filters, "modified": ["<", row.modified]},
+                "sum(quantity_change)",
+            )
+            or 0
+        )
+        row["quantity_after"] = row["quantity_before"] + row["quantity_change"]
+        row["unit_value"] = row["value_change"] / row["quantity_change"]
+
+    return columns, data
